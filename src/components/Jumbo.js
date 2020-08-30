edaa107958d2ba5261653dff1fcf5b4ef6064d4c
Suraj Verma <surajverma@rentalcars.com>
2020-08-30 14:04:30 +0100

    style jumbo component

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 const Jumbo = ({ title }) => {
   return (
     <div className="col-6 text-center p-4">
-      <div className="jumbotron">
+      <div
+        className="jumbotron d-flex align-items-center justify-content-center"
+        style={{ height: "300px" }}
+      >
         <h1 className="display-4">{title}</h1>
       </div>
     </div>
