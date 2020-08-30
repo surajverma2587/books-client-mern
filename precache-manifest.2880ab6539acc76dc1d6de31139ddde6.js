1d0e25571242fda7e09e1b677aa5cc55fb15fb37
Suraj Verma <surajverma@rentalcars.com>
2020-08-30 15:52:45 +0100

    Updates

---
+++ precache-manifest.2880ab6539acc76dc1d6de31139ddde6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a29e783cb511ab86dc3cad5d0863721",
+    "url": "/books-client-mern/index.html"
+  },
+  {
+    "revision": "bc7918b71e11e98debac",
+    "url": "/books-client-mern/static/js/2.0db90d1c.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/books-client-mern/static/js/2.0db90d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01aa25900c7ee0b64ea9",
+    "url": "/books-client-mern/static/js/main.6955cbe4.chunk.js"
+  },
+  {
+    "revision": "c87bad61e06ee5ff0bf9",
+    "url": "/books-client-mern/static/js/runtime-main.7340e32c.js"
+  }
+]);
